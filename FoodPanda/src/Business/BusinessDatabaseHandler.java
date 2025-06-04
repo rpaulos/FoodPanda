19c3871b9101c19d8387048179def8aab10208f6
Rae <r.paulos.dev@gmail.com>
2025-06-04 20:24:08 +0800

    Business Sign Up

--- FoodPanda/src/Business/BusinessDatabaseHandler.java
+++ FoodPanda/src/Business/BusinessDatabaseHandler.java
@@ -2,7 +2,6 @@ package Business;
 
 import Database.DatabaseCredentials;
 import java.sql.*;
-import java.time.Year;
 
 public class BusinessDatabaseHandler {
     private static BusinessDatabaseHandler handler = null;
@@ -86,8 +85,8 @@ public class BusinessDatabaseHandler {
         return false;
     }
 
-        // Checks if a business owner email exists in the database
-    public static boolean businessOwnerEmailExists(String oemail) {
+    // Checks if a business owner email exists in the database
+    public static boolean emailExists(String email) {
         getInstance();
 
         String query = "SELECT * FROM business_owner WHERE owner_email = ?";
@@ -95,7 +94,7 @@ public class BusinessDatabaseHandler {
         try (Connection conn = getDBConnection();
             PreparedStatement pstmt = conn.prepareStatement(query)) {
 
-            pstmt.setString(1, oemail);
+            pstmt.setString(1, email);
 
             ResultSet result = pstmt.executeQuery();
 
@@ -110,4 +109,198 @@ public class BusinessDatabaseHandler {
         return false;
     }
 
+    // Checks if company name exists in the database
+    public static boolean companyNameExists(String companyName) {
+        getInstance();
+
+        String query = "SELECT * FROM restaurant WHERE restaurant_name = ?";
+
+        try (Connection conn = getDBConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, companyName);
+
+            ResultSet result = pstmt.executeQuery();
+
+            if (result.next()) {
+             return true;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error checking business owner company name: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // Restaurant Location ID generator
+    public static String generateRestaurantLocationID(String address, String city, String zip) {
+        getInstance();
+
+        String cityCode = city.trim().toUpperCase().replaceAll("\\s+", "").substring(0, 3);
+        String prefix = "RL" + cityCode + "_";
+
+        String query = "SELECT restaurant_location_ID FROM restaurant_location WHERE restaurant_location_ID LIKE ? ORDER BY restaurant_location_ID DESC LIMIT 1";
+
+        try(Connection conn = getDBConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, prefix + "%");
+            ResultSet result = pstmt.executeQuery();
+
+            int nextNumber = 1;
+
+            if (result.next()) {
+                String lastID = result.getString("restaurant_location_ID");
+                String[] parts = lastID.split("_");
+                if (parts.length == 2) {
+                    nextNumber = Integer.parseInt(parts[1]) + 1;
+                }
+            }
+
+            return String.format("%s%05d", prefix, nextNumber);
+
+        } catch (SQLException e) {
+            System.out.println("Error getting restaurant location ID: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // Insert restaurant location into the database
+    public static void insertRestaurantLocation(String restaurantLocationID, String city, String address, String zip) {
+        getInstance();
+
+        String query = "INSERT INTO restaurant_location (restaurant_location_ID, city, address, zip_code) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = getDBConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, restaurantLocationID);
+            pstmt.setString(2, city);
+            pstmt.setString(3, address);
+            pstmt.setString(4, zip);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error inserting restaurant location: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // Generate restaurant ID
+    public static String generateRestaurantID(String restaurantLocationID) {
+        getInstance();
+
+        String prefix = "R_" + restaurantLocationID + "_";
+
+        String query = "SELECT restaurant_ID FROM restaurant WHERE restaurant_ID LIKE ? ORDER BY restaurant_ID DESC LIMIT 1";
+
+        try (Connection conn = getDBConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, prefix + "%");
+            ResultSet result = pstmt.executeQuery();
+
+            int nextNumber = 1;
+
+            if (result.next()) {
+                String lastID = result.getString("restaurant_ID");
+                String[] parts = lastID.split("_");
+                if (parts.length == 3) {
+                    nextNumber = Integer.parseInt(parts[2]) + 1;
+                }
+            }
+
+            return String.format("%s%03d", prefix, nextNumber);
+
+        } catch (SQLException e) {
+            System.out.println("Error generating restaurant ID: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // Insert restaurant into the database
+    public static void insertRestaurant(String restaurantID, String restaurantLocationID, String companyName) {
+        getInstance();
+
+        String query = "INSERT INTO restaurant (restaurant_ID, restaurant_location_ID, restaurant_name) VALUES (?, ?, ?)";
+
+        try (Connection conn = getDBConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, restaurantID);
+            pstmt.setString(2, restaurantLocationID);
+            pstmt.setString(3, companyName);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error inserting restaurant: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // Generate business owner ID
+    public static String generateBusinessOwnerID(String restaurantID) {
+        getInstance();
+
+        String prefix = "BO_" + restaurantID + "_";
+
+        String query = "SELECT business_owner_ID FROM business_owner WHERE business_owner_ID LIKE ? ORDER BY business_owner_ID DESC LIMIT 1";
+
+        try (Connection conn = getDBConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, prefix + "%");
+            ResultSet result = pstmt.executeQuery();
+
+            int nextNumber = 1;
+
+            if (result.next()) {
+                String lastID = result.getString("owner_ID");
+                String[] parts = lastID.split("_");
+                if (parts.length == 3) {
+                    nextNumber = Integer.parseInt(parts[2]) + 1;
+                }
+            }
+
+            return String.format("%s%03d", prefix, nextNumber);
+
+        } catch (SQLException e) {
+            System.out.println("Error generating business owner ID: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // Insert business owner into the database
+    public static void insertBusinessOwner(String ownerID, String restaurantID, String firstName, String lastName, String email, String password) {
+        getInstance();
+
+        String query = "INSERT INTO business_owner (business_owner_ID, restaurant_ID, owner_first_name, owner_last_name, owner_email, owner_password) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = getDBConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, ownerID);
+            pstmt.setString(2, restaurantID);
+            pstmt.setString(3, firstName);
+            pstmt.setString(4, lastName);
+            pstmt.setString(5, email);
+            pstmt.setString(6, password);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error inserting business owner: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
 }
