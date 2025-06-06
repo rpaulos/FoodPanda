9041bdc9167dc43b039d5adbc48a668f56162304
Rae <r.paulos.dev@gmail.com>
2025-06-06 11:10:06 +0800

    Restaurant items price_range

--- FoodPanda/src/Customer/Class/RestaurantItem.java
+++ FoodPanda/src/Customer/Class/RestaurantItem.java
@@ -5,12 +5,14 @@ public class RestaurantItem {
     private String restaurant_address;
     private String restaurant_header_path;
     private String restaurantID;
+    private String price_range;
 
-    public RestaurantItem(String restaurant_name, String restaurant_address, String restaurant_header_path, String restaurantID) {
+    public RestaurantItem(String restaurant_name, String restaurant_address, String restaurant_header_path, String restaurantID, String price_range) {
         this.restaurant_name = restaurant_name;
         this.restaurant_address = restaurant_address;
         this.restaurant_header_path = restaurant_header_path;
         this.restaurantID = restaurantID;
+        this.price_range = price_range;
     }
 
     public String getName() {
@@ -27,6 +29,10 @@ public class RestaurantItem {
 
     public String getRestaurantID() {
         return restaurantID;
+    }
+
+    public String getPriceRange() {
+        return price_range;
     }
 
 }
