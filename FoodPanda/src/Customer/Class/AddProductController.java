7492696f6bcaea58423e43a9c7e8b7b46a5baa29
Rae <r.paulos.dev@gmail.com>
2025-06-06 20:52:39 +0800

    AddProduct

---
+++ FoodPanda/src/Customer/Class/AddProductController.java
@@ -0,0 +1,39 @@
+package Customer.Class;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddProductController {
+
+    @FXML
+    private Button btn_BuyNow;
+
+    @FXML
+    private Button btn_account;
+
+    @FXML
+    private Button btn_add;
+
+    @FXML
+    private Button btn_addToBasket;
+
+    @FXML
+    private Button btn_back;
+
+    @FXML
+    private Button btn_food;
+
+    @FXML
+    private Button btn_grocery;
+
+    @FXML
+    private Button btn_search;
+
+    @FXML
+    private Button btn_subtract;
+
+    @FXML
+    private TextField tf_quantity;
+
+}
